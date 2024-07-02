1ed6e834570a16d8ce0d7a664fc45a1692613c67
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-07-02 23:20:27 +0300

    filter methods

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(drivers, name) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase() })
+  }
+
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+
+
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
